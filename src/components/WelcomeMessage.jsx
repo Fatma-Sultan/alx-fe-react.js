0beb499f9bc13f3a18068487110e65fb202fb360
fatma <fatmahassansultan97@gmail.com>
2024-08-11 22:19:49 +0300

    added some changes

--- src/components/WelcomeMessage.jsx
+++ src/components/WelcomeMessage.jsx
@@ -1,22 +1,11 @@
 function WelcomeMessage() {
     return (
-        <div>
-            <h1>Welcome to ALX React APP!</h1>
-            <p>This is a simple JSX component.</p>
-        </div>
+      <div>
+        <h1>Hello everyone, I am learning React at ALX!</h1>
+        <p>This is a simple JSX component.</p>
+        <p>I am learning about JSX!</p>
+      </div>
     );
-}
-
-export default WelcomeMessage;
-
-function WelcomeMessage() {
-    return (
-        <div>
-            <h1>Hello everyone, I am learning React at ALX!</h1>
-            <p>This is a simple JSX component.</p>
-            <p>I am learning about JSX!</p>
-        </div>
-    );
-}
-
-export default WelcomeMessage; 
+  }
+  
+  export default WelcomeMessage;
