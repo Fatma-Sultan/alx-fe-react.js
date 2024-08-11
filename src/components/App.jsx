04cd14c8671154cf94fb724d557e296889fc826b
fatma <fatmahassansultan97@gmail.com>
2024-08-11 22:12:43 +0300

    added some files

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,19 @@
+
+import Header from './components/Header';
+import MainContent from './components/MainContent';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <MainContent />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
---
+++ src/components/MainContent.jsx
@@ -0,0 +1,17 @@
+const MainContent = () => {
+    return (
+      <main>
+        {/* Your main content will go here */}
+        <p>Here are some of my favorite cities around the world:</p>
+        <ul>
+          <li>Tokyo</li>
+          <li>Paris</li>
+          <li>Cape Town</li>
+          <li>New York</li>
+          <li>Sydney</li>
+        </ul>
+      </main>
+    );
+  };
+  
+  export default MainContent;
