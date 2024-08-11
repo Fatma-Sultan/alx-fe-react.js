93d6bf6b4966120d5485621cee3d1fc095df7e92
fatma <fatmahassansultan97@gmail.com>
2024-08-11 22:36:20 +0300

    added some changes

--- src/src/components/App.jsx
+++ src/src/components/App.jsx
@@ -1,11 +1,34 @@
-function WelcomeMessage() {
+import React from 'react';
+import Header from './components/Header';
+import MainContent from './components/MainContent';
+import Footer from './components/Footer';
+import WelcomeMessage from './components/WelcomeMessage';
+import UserProfile from './components/UserProfile';
+
+function App() {
   return (
-    <div>
-      <h1>Hello everyone, I am learning React at ALX!</h1>
-      <p>This is a simple JSX component.</p>
-      <p>I am learning about JSX!</p>
+    <div className="App">
+      {/* Other components... */}
+      <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
     </div>
   );
 }
 
-export default WelcomeMessage;
+
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <MainContent />
+      <Footer />
+      <WelcomeMessage />
+    </div>
+  );
+}
+
+export default App;
+
+
+
+
---
+++ src/src/components/UserProfile.jsx
@@ -0,0 +1,11 @@
+const UserProfile = (props) => {
+    return (
+      <div>
+        <h2>{props.name}</h2>
+        <p>Age: {props.age}</p>
+        <p>Bio: {props.bio}</p>
+      </div>
+    );
+  };
+  
+  export default UserProfile;
