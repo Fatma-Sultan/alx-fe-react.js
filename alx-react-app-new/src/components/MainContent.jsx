26a5409792f45536b1a9ccce48220674ba73b1c1
fatma <fatmahassansultan97@gmail.com>
2024-08-18 23:20:34 +0300

    added some changes

--- alx-react-app-new/src/components/MainContent.jsx
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -5,7 +5,7 @@ const MainContent = () => {
   return (
     <main style={{ padding: '20px', backgroundColor: 'lightblue' }}>
       <h2>Welcome to the Main Content Area</h2>
-      <p>This is where the main content of the application goes.</p>
+      <p>["I love to visit New York, Paris, and Tokyo."].</p>
     </main>
   );
 };
